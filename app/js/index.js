64b9de1da70cf368fd3e84f9b7dbb4d95d456e36
alec <abenson@wpi.edu>
2015-09-15 12:58:16 -0400

    Cleanup of JS

--- app/js/index.js
+++ app/js/index.js
@@ -4,7 +4,40 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 });
 
 $(function() {
-  //Handle form submission events
+  bindSubmission();
+  getDataSources();
+  bindSubmissionButton();
+  bindFileSelectionText();
+  bindAjaxSpinner();
+});
+
+
+function bindAjaxSpinner() {
+  $(document).ajaxStart(function() {
+    $("#loading").fadeIn();
+  }).ajaxStop(function() {
+    $("#loading").fadeOut();
+  });
+}
+
+function getDataSources() {
+  $.ajax({
+    url: "/datasources",
+    type: "GET",
+    dataType: "JSON",
+    processData: false,
+    contentType: false,
+    success: function(data, status) {
+      var files = JSON.parse(data);
+      renderDatasourceBoxes(files);
+    },
+    error: function(xhr, desc, err) {
+      console.log("Failed: " + desc + err)
+    }
+  });
+};
+
+function bindSubmission() {
   $(document).on("submit", "#uploadform", function(event) {
     event.preventDefault();
     var url = $(this).attr("action");
@@ -16,12 +49,10 @@ $(function() {
       processData: false,
       contentType: false,
       success: function(data, status) {
-        var path = "/data/" + data.filename;
-
         //Remove previous KML entities
         viewer.entities.removeAll();
-
         //Load the KML object
+        var path = "/data/" + data.filename;
         viewer.dataSources.add(Cesium.KmlDataSource.load(path))
           .then(function(kmlData) { //success
               viewer.flyTo(kmlData.entities);
@@ -35,58 +66,27 @@ $(function() {
       error: function(xhr, desc, err) {}
     });
   });
+}
 
-  getDataSources();
-
-  //Bind submit button to form submission
+function bindSubmissionButton() {
   $(document).on("click", ".btn-submit", function() {
     $("#uploadform").submit();
   });
-
-  //When a file is selected, get the selection
-  $(document).on('change', '.btn-file :file', function() {
-    var input = $(this),
-      numFiles = input.get(0).files ? input.get(0).files.length : 1,
-      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-  });
-
-  //Result of file selection
-  $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-    $("#file-selection").html(label)
-  });
-
-  //Show/hide ajax spinner when uploading a file
-  $(document).ajaxStart(function() {
-    $("#loading").fadeIn();
-  }).ajaxStop(function() {
-    $("#loading").fadeOut();
-  });
-});
-
-function getDataSources() {
-  $.ajax({
-    url: "/datasources",
-    type: "GET",
-    dataType: "JSON",
-    processData: false,
-    contentType: false,
-    success: function(data, status) {
-      console.log("Data:" + data)
-      var files = JSON.parse(data);
-      renderDatasourceBoxes(files);
-    },
-    error: function(xhr, desc, err) {
-      console.log("Failed: " + desc + err)
-    }
-  });
-};
+}
 
 function renderDatasourceBoxes(files) {
-  $("#datasources").empty();
   var dataDiv = $("#datasources");
+  $(dataDiv).empty();
   $.each(files, function(index, file) {
     var check = "<div class='checkbox'><label><input type='checkbox'>" + file + "</label></div>";
     $(dataDiv).append(check);
+  });
+}
+
+function bindFileSelectionText() {
+  $(document).on('change', '.btn-file :file', function() {
+    var input = $(this);
+    var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    $("#file-selection").html(label)
   });
 }
