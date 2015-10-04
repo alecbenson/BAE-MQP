bb689eca3a8c271e8613876b3884b03b678b4347
alec <abenson@wpi.edu>
2015-10-04 13:09:39 -0400

    Progress on sample interpolation

--- app/js/trackSource.js
+++ app/js/trackSource.js
@@ -1,11 +1,11 @@
 var TrackDataSource = function(name) {
 
   this._name = name;
-  this._entityCollection = new Cesium.EntityCollection,
-    this._clock = new Cesium.DataSourceClock();
-  this._clock.startTime = Cesium.JulianDate.fromIso8601("2015-09-30");
-  this._clock.stopTime = Cesium.JulianDate.fromIso8601("2015-10-01");
-  this._clock.currentTime = Cesium.JulianDate.fromIso8601("2015-09-30");
+  this._entityCollection = new Cesium.EntityCollection;
+  this._clock = new Cesium.DataSourceClock();
+  this._clock.startTime = Cesium.JulianDate.fromIso8601("2015-10-03");
+  this._clock.stopTime = Cesium.JulianDate.fromIso8601("2015-10-04");
+  this._clock.currentTime = Cesium.JulianDate.fromIso8601("2015-10-03");
   this._clock.clockRange = Cesium.ClockRange.LOOP_STOP;
   this._clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER;
   this._clock.multiplier = 1;
@@ -94,6 +94,7 @@ TrackDataSource.prototype.loadUrl = function(url) {
   });
 };
 
+//Connects two vertices together
 TrackDataSource.prototype.connect = function(first, other) {
   var edgeName = first.id + " " + other.id;
   var orangeOutlined = viewer.entities.add({
@@ -108,45 +109,42 @@ TrackDataSource.prototype.connect = function(first, other) {
   });
 }
 
+TrackDataSource.prototype.addSample = function(entities, data) {
+  var property = new Cesium.SampledPositionProperty();
+  var position = Cesium.Cartesian3.fromDegreesArrayHeights([data.lon, data.lat, data.ele]);
+  var time = Cesium.JulianDate.fromIso8601(data.time);
+  property.addSample(time, position);
+
+  //Create a point for the sample data
+  var entity = entities.add({
+    position: Cesium.Cartesian3.fromDegrees(data.lon, data.lat),
+    point: {
+      pixelSize: 10,
+      color: Cesium.Color.BLUE,
+      outlineColor: Cesium.Color.CYAN,
+      outlineWidth: 2
+    }
+  });
+  return property;
+}
+
 TrackDataSource.prototype.load = function(data) {
   if (!Cesium.defined(data)) {
     throw new Cesium.DeveloperError('data is required.');
   }
   this._setLoading(true);
-
   var heightScale = this.heightScale;
   var entities = this._entityCollection;
-
   entities.suspendEvents();
   entities.removeAll();
 
-  for (var i = 0; i < data.length; i++) {
-    var trackData = data[i];
-    var lat = trackData.lat;
-    var lon = trackData.lon;
-    var ele = trackData.ele;
-    var id = trackData.id
-    var time = trackData.time
-
-    //Draw the vertice on the map
-    var pinBuilder = new Cesium.PinBuilder();
-    entities.add({
-      id: id,
-      name: "Point " + id,
-      position: Cesium.Cartesian3.fromDegrees(lon, lat),
-      billboard: {
-        image: pinBuilder.fromColor(Cesium.Color.CYAN, 16).toDataURL(),
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-      }
-    });
-
-    //Connect vertices with polylines
-    if(i >= 0 && i < (data.length - 1)) {
-      var next = data[i + 1];
-      TrackDataSource.prototype.connect(trackData, next);
-    }
-
+  //Iterate through each vertice, and add a sample point
+  for (var i = 0; i < data.vertices.length; i++) {
+    var trackData = data.vertices[i];
+    TrackDataSource.prototype.addSample(entities, trackData);
   }
+
+  //Handle appropriate cesium events
   entities.resumeEvents();
   this._changed.raiseEvent(this);
   this._setLoading(false);
--- generator.py
+++ generator.py
@@ -5,50 +5,79 @@ import math
 from time import gmtime, strftime
 from datetime import date, datetime, time, timedelta
 
+
 class Generator:
+
     def __init__(self):
-        self.points = int(raw_input("How many points of data would you like to generate? "))
-        self.data = []
-        self.json = self._generate()
-        self._dump_json_to_file()
+        self.points = int(
+            raw_input("How many points of data would you like to generate? "))
+        self.radius = int(raw_input(
+            "How large -- in geographic degrees -- should the circular track be? "))
+
+        self.vertices = self._generate_vertices()
+        self.edges = self._generate_edges()
+        self.data = self._dump_json_to_file()
+
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        del state['radius']
+        del state['points']
+        return state
 
     def _format_time(self, time_struct, offset):
         delta = timedelta(seconds=offset)
         new = datetime.combine(date.today(), time()) + delta
         return new.strftime("%Y-%m-%dT%H:%M:%S")
 
-    def _generate(self):
+    def _generate_vertices(self):
         vertices = []
-        start_lat = random.uniform(0.0,90.0)
-        start_lon = random.uniform(0.0,179.0)
+        start_lat = random.uniform(0.0, 90.0)
+        start_lon = random.uniform(0.0, 179.0)
         start_time = gmtime()
-        start_ele = random.randrange(0,6000)
+        start_ele = random.randrange(0, 6000)
 
-        #How big of a circle to move in
-        degree_radius = random.uniform(0.5,3.0)
-        #How many degrees each point should move within the circle
-        degree_step = math.radians(360.0/self.points)
+        # How many degrees each point should move within the circle
+        degree_step = math.radians(360.0 / self.points)
 
         for x in range(self.points):
-            start_ele = start_ele + random.randint(-10,10)
-            wobble_x = random.uniform(0.9,1.0)
-            wobble_y = random.uniform(0.9,1.0)
+            start_ele = start_ele + random.randint(-10, 10)
+            wobble_x = random.uniform(0.9, 1.0)
+            wobble_y = random.uniform(0.9, 1.0)
 
-            lat_offset = math.sin(degree_step * x) * degree_radius * wobble_y
-            lon_offset = math.cos(degree_step * x) * degree_radius * wobble_x
+            lat_offset = math.sin(degree_step * x) * self.radius * wobble_y
+            lon_offset = math.cos(degree_step * x) * self.radius * wobble_x
 
-            vert = Vertice(start_lat + lat_offset, start_lon + lon_offset, start_ele, self._format_time(start_time, x))
+            vert = Vertice(start_lat + lat_offset, start_lon +
+                           lon_offset, start_ele, self._format_time(start_time, x))
             vertices.append(vert)
 
-        #Return json formatted data
-        return json.dumps(vertices, default=lambda o: o.__dict__)
+        # Return json formatted data
+        return vertices
+
+    def _generate_edges(self):
+        edges = []
+
+        if not self.vertices:
+            self.vertices = self._generate_vertices()
+
+        for index in range(len(self.vertices)):
+            if index == len(self.vertices) - 1:
+                break
+
+            id_a = self.vertices[index].id
+            id_b = self.vertices[index + 1].id
+            weight = random.uniform(1.0, 3.0)
+            edge = Edge(id_a, id_b, weight)
+            edges.append(edge)
+        return edges
 
     def _dump_json_to_file(self):
         with open('sample.json', 'w') as outfile:
-            outfile.write(self.json)
-
+            json_data = json.dumps(self, default=lambda o: o.__dict__)
+            outfile.write(json_data)
 
 class Vertice(object):
+
     def __init__(self, lat, lon, ele, time):
         self.id = random.getrandbits(64)
         self.lat = lat
@@ -59,6 +88,18 @@ class Vertice(object):
     def __repr__(self):
         return "ID:{0}, LAT:{1} LON:{2} ELEV:{3} TIME:{4}" \
             .format(self.id, self.lat, self.lon, self.ele, self.time)
+
+
+class Edge(object):
+
+    def __init__(self, id_a, id_b, weight):
+        self.id_a = id_a
+        self.id_b = id_b
+        self.weight = weight
+
+    def __repr__(self):
+        return "(Edge connecting {0} to {1})" \
+            .format(self.id_a, self.id_b)
 
 
 if __name__ == "__main__":
