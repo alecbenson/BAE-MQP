969a5945f29dbc906279fd8346e702a565b53478
zoccihedron <pelynch@wpi.edu>
2015-10-28 21:40:27 -0400

    Modified chart.js to work with our data. pelynch

--- app/js/chart.js
+++ app/js/chart.js
@@ -3,11 +3,11 @@
     "use strict";
 
     // Various accessors that specify the four dimensions of data to visualize.
-    function x(d) { return d.income; }
-    function y(d) { return d.lifeExpectancy; }
-    function radius(d) { return d.population; }
-    function color(d) { return d.region; }
-    function key(d) { return d.name; }
+    function x(d) { return d.lon; }
+    function y(d) { return d.lat; }
+    function radius(d) { return d.ele; }
+    // function color(d) { return d.; }
+    function key(d) { return d.id; }
 
     // Chart dimensions.
     var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
@@ -15,10 +15,10 @@
         height = 500 - margin.top - margin.bottom;
 
     // Various scales. These domains make assumptions of data, naturally.
-    var xScale = d3.scale.log().domain([300, 1e5]).range([0, width]),
-        yScale = d3.scale.linear().domain([10, 85]).range([height, 0]),
-        radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
-        colorScale = d3.scale.category20c();
+    var xScale = d3.scale.linear().domain([-180, 180]).range([0, width]),
+        yScale = d3.scale.linear().domain([-90, 90]).range([height, 0]),
+        radiusScale = d3.scale.sqrt().domain([0, 50000]).range([0, 40]);
+        // colorScale = d3.scale.category20c();
 
     // The x & y axes.
     var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
@@ -48,7 +48,7 @@
         .attr("text-anchor", "end")
         .attr("x", width)
         .attr("y", height - 6)
-        .text("income per capita, inflation-adjusted (dollars)");
+        .text("longitude (degrees)");
 
     // Add a y-axis label.
     svg.append("text")
@@ -57,7 +57,7 @@
         .attr("y", 6)
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
-        .text("life expectancy (years)");
+        .text("latitude (degrees)");
 
     // Add the year label; the value is set on transition.
     var label = svg.append("text")
@@ -65,16 +65,17 @@
         .attr("text-anchor", "start")
         .attr("y", 28)
         .attr("x", 30)
-        .text(1800);
+        .text("test title");
 
     // Load the data.
-    d3.json("nations_geo.json", function(nations) {
+    d3.json("sample.json", function(vertices) {
 
       // A bisector since many nation's data is sparsely-defined.
       var bisect = d3.bisector(function(d) { return d[0]; });
 
       // Positions the dots based on data.
       function position(dot) {
+        console.log(dot);
         dot .attr("cx", function(d) { return xScale(x(d)); })
             .attr("cy", function(d) { return yScale(y(d)); })
             .attr("r", function(d) { return radiusScale(radius(d)); });
@@ -85,16 +86,14 @@
         return radius(b) - radius(a);
       }
       // Interpolates the dataset for the given (fractional) year.
-      function interpolateData(year) {
-        sharedObject.yearData = nations.map(function(d) {
+      function interpolateData() {
+        sharedObject.yearData = vertices.vertices.map(function(d) {
           return {
-            name: d.name,
-            region: d.region,
-            income: interpolateValues(d.income, year),
-            population: interpolateValues(d.population, year),
-            lifeExpectancy: interpolateValues(d.lifeExpectancy, year),
+            id: d.id,
             lat: d.lat,
-            lon: d.lon
+            lon: d.lon,
+            ele: d.ele,
+            time: d.time
           };
         });
 
@@ -105,10 +104,10 @@
       var dot = svg.append("g")
           .attr("class", "dots")
         .selectAll(".dot")
-          .data(interpolateData(1800))
+          .data(interpolateData())
         .enter().append("circle")
           .attr("class", "dot")
-          .style("fill", function(d) { return colorScale(color(d)); })
+        //  .style("fill", function(d) { return colorScale(color(d)); })
           .call(position)
           .sort(order)
 		  .on("mouseover", function(d) {
