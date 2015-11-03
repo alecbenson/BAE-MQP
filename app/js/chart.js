ca45c3459c7d0cd837ae43bca2194e2ba17d027f
alec <abenson@wpi.edu>
2015-11-02 19:56:54 -0500

    got basic chart working

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,67 +1,76 @@
 /* global d3 */
 
-(function () {
-    // Chart dimensions.
-    var width = 500,
-        height = 500;
+(function() {
+  // Chart dimensions.
+  var width = 500,
+    height = 500;
 
-    // Set the force between vertices
-    var force = d3.layout.force()
-        .size([width, height]) // Sets the size of the chart
-        .charge(-400) // The strength of the force
-        .linkDistance(40) // The distance between 2 nodes
-        .on("tick", tick); // When to update
+  // Set the force between vertices
+  var force = d3.layout.force()
+    .size([width, height]) // Sets the size of the chart
+    .charge(-400) // The strength of the force
+    .linkDistance(40) // The distance between 2 nodes
+    .on("tick", tick); // When to update
 
-    // Make vertices dragable
-    var drag = force.drag()
-        .on("dragstart", dragstart);
+  // Make vertices dragable
+  var drag = force.drag()
+    .on("dragstart", dragstart);
 
-    // Create the SVG container and set the origin.
-    var svg = d3.select("#chart").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+  // Create the SVG container and set the origin.
+  var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-    // Get a list of the links and nodes
-    var link = svg.selectAll(".link"),
-        node = svg.selectAll(".node");
+  // Get a list of the links and nodes
+  var link = svg.selectAll(".link"),
+    node = svg.selectAll(".node");
 
-    d3.json("graph.json", function(error, graph) {
-        if(error) {
-            throw error;
-        }
-
-        force
-            .nodes(graph.nodes)
-            .links(graph.links)
-            .start();
-
-        link = link.data(graph.links)
-            .enter().append("line")
-            .attr("class", "link");
-
-        node = node.data(graph.nodes)
-            .enter().append("circle")
-            .attr("class", "node")
-            .attr("r", 12)
-            .on("dblclick", dblclick)
-            .call(drag);
-    });
-
-    function tick() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.source.x; })
-            .attr("y2", function(d) { return d.source.y; });
-
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+  d3.json("graph.json", function(error, graph) {
+    if (error) {
+      throw error;
     }
+    force
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .start();
 
-    function dblclick(d) {
-        d3.select(this).classed("fixed", d.fixed = false);
-    }
+    link = link.data(graph.links)
+      .enter().append("line")
+      .attr("class", "link");
 
-    function dragstart(d) {
-        d3.select(this).classed("fixed", d.fixed = true);
-    }
+    node = node.data(graph.nodes)
+      .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 12)
+      .on("dblclick", dblclick)
+      .call(drag);
+  });
+
+  function tick() {
+    link.attr("x1", function(d) {
+        return d.source.x;
+      })
+      .attr("y1", function(d) {
+        return d.source.y;
+      })
+      .attr("x2", function(d) {
+        return d.target.x;
+      })
+      .attr("y2", function(d) {
+        return d.target.y;
+      });
+
+    node.attr("cx", function(d) {
+        return d.x;
+      })
+      .attr("cy", function(d) {
+        return d.y;
+      });
+  }
+
+  function dblclick(d) {
+  }
+
+  function dragstart(d) {
+  }
 }());
