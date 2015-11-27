6afd9ed92e130134329e08beb3f5888f65ac797b
Alec Benson <abenson@wpi.edu>
2015-11-26 21:36:16 -0500

    track history has pips now. also added plumbing for history

--- app/js/history.js
+++ app/js/history.js
@@ -1,7 +1,9 @@
 function HistorySlider(start, stop) {
+  var outerScope = this;
+
   this._start = start;
   this._stop = stop;
-  this._init();
+  this._renderSlider();
 }
 
 Object.defineProperties(HistorySlider.prototype, {
@@ -20,24 +22,44 @@ Object.defineProperties(HistorySlider.prototype, {
     set: function(stop) {
       this._stop = stop;
     }
+  },
+  'slider': {
+    get: function() {
+      return this._slider;
+    },
+    set: function(slider) {
+      this._slider = slider;
+    }
   }
 });
 
-HistorySlider.prototype._init = function() {
+HistorySlider.prototype._renderSlider = function() {
+  var d = $.Deferred();
   var outerScope = this;
   getTemplateHTML('history-slider').done(function(data) {
     var templated = applyTemplate(data);
     $(templated).appendTo(document.body);
     outerScope._makeSlider();
+    d.resolve(outerScope);
   });
+  return d;
 }
 
 HistorySlider.prototype._sliderStartStop = function(pct) {
   var diff = this.stop - this.start;
-  pct = (pct/100.0);
+  pct = (pct / 100.0);
   var newStart = (diff * pct) - Math.abs(this.stop);
   var newStop = (diff * (1.0 - pct)) - Math.abs(this.start);
   return [newStart, newStop];
+}
+
+HistorySlider.prototype.getValues = function() {
+  return this.slider.get();
+}
+
+HistorySLider.prototype._updateTrackHistory = function() {
+  this.slider.noUiSlider.on('change', function(values, handle) {};
+  }
 }
 
 HistorySlider.prototype._makeSlider = function() {
@@ -51,14 +73,27 @@ HistorySlider.prototype._makeSlider = function() {
       'min': this.start,
       'max': this.stop
     },
+    pips: {
+      mode: 'positions',
+      values: [0, 25, 50, 75, 100],
+      density: 10
+    },
     format: wNumb({
       decimals: 0,
-      postfix: 's'
+      postfix: 'sec'
     })
   });
-  slider.noUiSlider.on('set', function() {
-    var vals = slider.noUiSlider.get();
-    var start = parseInt(vals[0]);
-    var stop = parseInt(vals[1]);
+  this.slider = slider;
+  this._setSoftLimits();
+}
+
+HistorySlider.prototype._setSoftLimits = function() {
+  var outerScope = this;
+  this.slider.noUiSlider.on('change', function(values, handle) {
+    if (parseInt(values[0]) > 0) {
+      outerScope.slider.noUiSlider.set([0, null]);
+    } else if (parseInt(values[1]) < 0) {
+      outerScope.slider.noUiSlider.set([null, 0]);
+    }
   });
 }
